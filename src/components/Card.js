e7f74d502015e3cd732c85ac5a84b39046e3af0c
Tejus Revi <tejusrevi@gmail.com>
2020-08-21 07:55:03 -0400

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ const Card = (props) =>{
     return(
         <div className="card">
             <button className="card-button">
-                <div className="static-thumb" style= {{backgroundImage:'url(https://maps.googleapis.com/maps/api/streetview?size=400x400&location=47.5763831,-122.4211769&fov=80&heading=70&pitch=0&key=AIzaSyCnunObqBOdXA59S5Q11a0GXzTkJDC5bs0)'}}></div>
+                <div className="static-thumb" style= {{backgroundImage:'url(https://maps.googleapis.com/maps/api/streetview?size=400x400&location=47.5763831,-122.4211769&fov=80&heading=70&pitch=0&key=YOUR API)'}}></div>
     <div className="added-by">{props.data.author}</div>
             </button>
         </div>
