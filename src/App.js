5452e4e5a501ad3cad57e6c0cc327867f339fd1d
Tejus <tejusrevi@gmail.com>
2020-08-21 07:53:10 -0400

    Finished Menu

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 
+import {Menu} from './components/Menu.js';
+import {Card} from './components/Card.js';
+
+import * as firebase from "firebase/app";
+import "firebase/analytics";
+import "firebase/firestore";
+import "firebase/database";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAx65bSZGyrNGTP07-jdNGkjKYu_FoPYTI",
+  authDomain: "map-o-gram.firebaseapp.com",
+  databaseURL: "https://map-o-gram.firebaseio.com",
+  projectId: "map-o-gram",
+  storageBucket: "map-o-gram.appspot.com",
+  messagingSenderId: "1047462531795",
+  appId: "1:1047462531795:web:22fa8cde942cca08903a26",
+  measurementId: "G-LZFV7WVYQL"
+};
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+/*
+const dRefObject = firebase.database().ref().child('object');
+
+firebase.database().ref().child('object').push({
+  latitude: 5,
+  longitude: 9,
+  heading : 0,
+  pitch: 0,
+  author: 'Geethika Revi'
+});
+
+
+dRefObject.on('value', snap=>console.log(snap.val()))
+*/
+
+var locationArr = [];
+
+
+
+
 function App() {
+  
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    firebase.database().ref('object').on('value', (snap)=>{
+      locationArr = [];
+      Object.values(snap.val()).forEach(e=>{
+        locationArr.push(e)
+      })
+      setData(locationArr);
+    })
+  },[]);
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="menu-container">
+        <Menu/>
+      </div>
+      <div id="content">
+        {data.map(e =>
+          <Card data={e}/>
+        )}
+          
+      </div>
     </div>
   );
 }
---
+++ src/components/Menu.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Menu = () =>{
+    return(
+    <div id="menu-bar">
+        <header id="header">
+            <div id="home">
+                MAPOGRAM
+            </div>
+            <div id="caption">
+                AMAZING PLACES AROUND THE WORLD 🌍
+            </div>
+        </header>
+        <div id="menu">
+            <div className="menu-item">
+                <button id="me-button" className="menu-button" onClick={()=>window.open('http://tejus-revi.web.app/', '_blank')}>ME</button>
+            </div>
+            <div className="menu-item">
+                <button className="menu-button">DATABASE</button>
+            </div>
+            <div className="menu-item">
+                <button className="menu-button" >HOME</button>
+            </div>
+            
+        </div>
+    </div>
+    )
+    
+}
+
+export {Menu};
